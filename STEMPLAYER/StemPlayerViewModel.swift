00ffea2fa3486c72537c31ee5e7e48ec2b49b534
Igor Łopatka <igor.lopatka11@gmail.com>
2022-12-21 22:20:17 +0100

    update vm with players and files []

--- STEMPLAYER/StemPlayerViewModel.swift
+++ STEMPLAYER/StemPlayerViewModel.swift
@@ -5,13 +5,22 @@
 //  Created by Igor Łopatka on 21/12/2022.
 //
 
+import AVFoundation
 import SwiftUI
 
-class StemPlayerViewModel: ObservableObject {
+@MainActor class StemPlayerViewModel: ObservableObject {
     
+    @Published var importedFiles: [URL] = []
+    @Published var audioPlayers: [AVAudioPlayer] = []
     
-    
-    
-    
-    
+    func importFiles(results: [Result<URL, Error>]) {
+        for result in results {
+            switch result {
+            case .success(let url):
+                importedFiles.append(url)
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
 }
