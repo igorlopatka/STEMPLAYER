d1edbfac24e7c301528a969015594e5f2e15230c
Igor Łopatka <igor.lopatka11@gmail.com>
2022-12-21 22:15:31 +0100

    create vm

---
+++ STEMPLAYER/StemPlayerViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  StemViewModel.swift
+//  STEMPLAYER
+//
+//  Created by Igor Łopatka on 21/12/2022.
+//
+
+import SwiftUI
+
+class StemPlayerViewModel: ObservableObject {
+    
+    
+    
+    
+    
+    
+}
