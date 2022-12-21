60b9eb7211924e6f80b2653d12f91ddb51329ac2
Igor Łopatka <igor.lopatka11@gmail.com>
2022-12-21 22:11:01 +0100

    Initial Commit

---
+++ STEMPLAYER/STEMPLAYERApp.swift
@@ -0,0 +1,17 @@
+//
+//  STEMPLAYERApp.swift
+//  STEMPLAYER
+//
+//  Created by Igor Łopatka on 21/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct STEMPLAYERApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
