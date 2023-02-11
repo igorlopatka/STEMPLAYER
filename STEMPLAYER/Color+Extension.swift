a1381274653016ab7b6284adf9fa4ba9e7f07f39
Igor Łopatka <igor.lopatka11@gmail.com>
2023-02-11 12:16:13 +0100

    Add SwiftUI Color extension

--- STEMPLAYER/Color+Extension.swift
+++ STEMPLAYER/Color+Extension.swift
@@ -1,10 +1,13 @@
 //
-//  Colors+Extension.swift
+//  Color+Extension.swift
 //  STEMPLAYER
 //
 //  Created by Igor Łopatka on 11/02/2023.
 //
 
-import Foundation
+import SwiftUI
 
-extension Color
+extension Color {
+    static let light = Color("light")
+    static let dark = Color("dark")
+}
